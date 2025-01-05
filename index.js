513dfeaf3d6dbaf6e892c62c6a7efcf2700b00fc
limlleonard <limlleonard@gmail.com>
2025-01-05 19:46:48 +0100

    test2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-
+console.log('test2');
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
